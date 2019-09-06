d1afed39dc955f1855b8a33f65715c347bb0c91d
Gabriel Tomberlin <gabrieltomberlin14@gmail.com>
2019-09-05 22:59:22 -0500

    Update get_list_totals.py

--- get_list_totals.py
+++ get_list_totals.py
@@ -1,5 +1,5 @@
-weak = "C:\\Users\\g1jgt01\\.spyder-py3\\PW_Predictor\\weak_master_list.txt"
-strong = "C:\\Users\\g1jgt01\\.spyder-py3\\PW_Predictor\\strong_master_list.txt"
+weak = "weak_master_list.txt"
+strong = "strong_master_list.txt"
 
 total = 0
 
